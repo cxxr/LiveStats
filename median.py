a67552cc6b72e82a7ab56efc62fe1385ced24548
Sean Cassidy <sean.a.cassidy@gmail.com>
2012-04-18 13:59:47 -0700

    Add Triangular and Bimodal distributions

--- median.py
+++ median.py
@@ -28,6 +28,14 @@ def output(data, estimate, stype):
 
     print "{0}: Estimated: {1}, Actual: {2}, Avg: {3}, %Error {4}".format(stype, estimate, med, avg, pe)
 
+def bimodal( low1, high1, mode1, low2, high2, mode2 ):
+    toss = random.choice( (1, 2) )
+    if toss == 1:
+        return random.triangular( low1, high1, mode1 ) 
+    else:
+        return random.triangular( low2, high2, mode2 )
+
+
 if __name__ == '__main__':
     count = int(sys.argv[1])
     random.seed()
@@ -46,5 +54,20 @@ if __name__ == '__main__':
         median.add(x[i])
 
     output(x, median.median, "Random")
+
+    median = MovingMedian()
+    for i in range(count):
+        x[i] = random.triangular(-1000, 1000, 999)
+        median.add(x[i])
+
+    output(x, median.median, "Triangular")
+
+    median = MovingMedian()
+    for i in range(count):
+        x[i] = bimodal(0, 1000, 500, 500, 1500, 1400)
+        median.add(x[i])
+
+    output(x, median.median, "Bimodal")
+
 
 
