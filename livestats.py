b76075a9ca1f5659e571673f33b287d056780012
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-03-05 19:58:22 -0800

    Closer, but it still isn't right
    The algorithm for the new positions isn't correct

--- livestats.py
+++ livestats.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-from array import array
-from math import fabs,copysign
+from math import copysign
 import random, sys
 
-def calcP2(q, qm1, d, np1, n, nm1):
+def calcP2(qp1, q, qm1, d, np1, n, nm1):
     outer = d / ( np1 - nm1 )
     inner_left = ( n - nm1 + d ) * ( qp1 - q ) / ( np1 - n )
     inner_right = ( np1 - n - d ) * ( q - qm1 ) / ( n - nm1 )
@@ -17,46 +16,61 @@ class LiveHistogram:
 
     def __init__(self, num_cells = 10):
         self.num_cells = num_cells + 1
-        self.pos = list(range(num_cells))
+        self.pos = list(range(1, num_cells + 2))
+        self.npos = list(range(1, num_cells + 2))
+        self.i = self.num_cells
 
     def add(self, item):
-        if len(self.heights) < self.num_cells + 1:
+        if len(self.heights) < self.num_cells:
             self.heights.append(item)
         else:
             if self.initialized == False:
-                self.heights.sort
+                self.heights.sort()
                 self.initialized = True
 
             # find cell k
             if self.heights[0] > item:
                 self.heights[0] = item
-                k = 0
+                k = 1
             else:
                 for i in range(1, len(self.heights) - 1):
                     if self.heights[i] < item and item < self.heights[i + 1]:
-                        k = i
+                        k = i + 1
                         break
                 else:
-                    k = self.num_cells - 1
+                    k = self.num_cells
+                    self.pos[-1] = self.pos[-1] + 1
                     if self.heights[-1] < item:
                         self.heights[-1] = item
 
             # increment all positions greater than k
-            self.pos = self.pos[:k] + [x + 1 for x in self.pos[k:]]
-            self.__adjust
+            if k < len(self.pos):
+                self.pos = self.pos[:k] + [x + 1 for x in self.pos[k:]]
+
+            copyn = self.pos[:]
+            desired = self.__adjust()
+            self.i = self.i + 1
+            #print "{}\t{}\t{}\t{}\t\t{}".format(self.i, item, k, copyn, desired)
 
     def __adjust(self):
+        desired = []
+        desired.append(self.pos[0])
         for i in range(1, self.num_cells - 1):
             n = self.pos[i]
             q = self.heights[i]
-            nn = 1.0 + (i - 1.0)*(n - 1.0)/(self.num_cells - 1.0)
+            self.npos[i] = self.npos[i] + i*(n - self.pos[i - 1])/(self.num_cells - 1.0)
+            nn = self.npos[i]
+
+            desired.append(nn)
+
             dist = nn - n
             back_dist = self.pos[i - 1] - n
             forw_dist = self.pos[i + 1] - n
 
             if (dist >= 1 and forw_dist > 1) or (dist <= -1 and back_dist < -1):
-                dist = copysign(1, dist)
-                qn = calcP2(q, self.heights[i - 1], dist, self.pos[i + 1], n, self.pos[i - 1])
+                dist = int(copysign(1, dist))
+                qn = calcP2(self.heights[i + 1], q, self.heights[i - 1], dist, self.pos[i + 1], n, self.pos[i - 1])
+
                 if self.heights[i - 1] < qn and qn < self.heights[i + 1]:
                     self.heights[i] = qn
                 else:
@@ -64,6 +78,9 @@ class LiveHistogram:
                     self.heights[i] = q + dist * (self.heights[i + dist] - q) / (self.pos[i + dist] - n)
 
                 self.pos[i] = n + dist
+
+        desired.append(self.pos[-1])
+        return desired
 
     def histogram(self):
         if self.initialized == True:
@@ -91,7 +108,8 @@ if __name__ == '__main__':
     random.seed()
 
     median = LiveHistogram(4)
-    test = [1, 2, 3, 4, 5, 6, 22.37, 10.15, 15.43]
+    test = [0.2, 0.5, 0.74, 3.39, 0.83, 22.37, 10.15, 15.43, 38.62, 15.92, 34.60,
+            10.28, 1.47, 0.40, 0.05, 11.39, 0.27, 0.42, 0.09, 11.37]
     for i in test:
         median.add(i)
 
