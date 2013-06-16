54d5c00bfa9d5a42fce698c14f32c8cf345d47cf
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-06-16 10:31:32 -0700

    Use setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='LiveStats',
+      version='1.0',
+      description='LiveStats solves the problem of generating accurate statistics for when your data set is too large to fit in memory, or too costly to sort.',
+      author='Sean Cassidy',
+      author_email='gward@python.net',
+      url='https://bitbucket.org/scassidy/livestats',
+      packages=['livestats'],
+     )
